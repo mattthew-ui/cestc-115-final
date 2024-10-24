fad0fe54101d75c4e21ae3c9473af28f45b3fd9e
mattthew-ui <MatthewBerryhill22@outlook.com>
2024-10-24 10:41:29 -0400

    add some js

--- tictaketoe.js
+++ tictaketoe.js
@@ -1,6 +1,14 @@
 let player = 'x'
-const board = Array(9),full(null);
+const board = Array(9).full(null);
 
 function yourturn(index){
-    const
+    const buttons = document.querySelectorAll('.btn-container button')
+    if (!board[index]){
+        board[index] = currentPlayer
+        buttons[index].textContent = currentPlayer
+
+        if(checkWinner()){
+            
+        }
+    }
 }
