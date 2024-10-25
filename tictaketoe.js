b3c401b3af5fca46c6f1cb4e94c17c204ae794f9
Jacob Rushinski <dsrush2@verizon.net>
2024-10-25 10:42:40 -0400

    win logic progress

--- tictaketoe.js
+++ tictaketoe.js
@@ -1,13 +1,57 @@
 let check = 1
 
 function clicked(selected) {
-    if (check === 1) {
-        const element = document.querySelector('#' + selected.id);
-        element.textContent = 'X';
-        check++;
-    } else {
-        const element = document.querySelector('#' + selected.id);
-        element.textContent = 'O';
-        check--;
-    }   
+    const btnsLoc = document.querySelector('.btn-container');
+    const btns = [...btnsLoc.querySelectorAll("button")];
+
+    const winLoc = [
+        [ btns[0].value, btns[1].value, btns[2].value ],
+        [ btns[3].value, btns[4].value, btns[5].value ],
+        [ btns[6].value, btns[7].value, btns[8].value ],
+        [ btns[0].value, btns[3].value, btns[6].value ],
+        [ btns[1].value, btns[4].value, btns[7].value ],
+        [ btns[2].value, btns[5].value, btns[8].value ],
+        [ btns[0].value, btns[4].value, btns[8].value ],
+        [ btns[2].value, btns[4].value, btns[6].value ]
+    ];
+
+    for (let y = 0; y < winLoc.length - 1; y++) {
+        const vars = [ 'X', 'O'];
+        for (let i = 0; i < vars.length - 1; i++) {
+            if (winLoc[y][0] === vars[i] && winLoc[y][1] === vars[i] && winLoc[y][2] === vars[i])  {
+                console.log(vars[i]);
+                console.log('test');
+            }
+        }
+    }
+
+
+    // for (let i = 0; i < btns.length; i++) {
+    //     let boardCheck = btns[i];
+    //     checkWin(boardCheck);
+    // }
+
+    //if () {
+
+    //} else {
+        if (selected.value === 'X' || selected.value === 'O') {
+            alert(`The space you selected already contains a ${selected.textContent} already, please select a different space`);
+        } else {
+            if (check === 1) {
+                const element = document.querySelector('#' + selected.id);
+                element.value = 'X';
+                element.textContent = 'X';
+                check++;
+            } else {
+                const element = document.querySelector('#' + selected.id);
+                element.value = 'O';
+                element.textContent = 'O';
+                check--;
+            }   
+        }
+    //} 
+}
+
+function checkWin(boardCheck) {
+    // console.log(boardCheck);
 }
