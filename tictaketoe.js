8622069c0c5c450b680cb6a3a0b509ac730d7085
mattthew-ui <MatthewBerryhill22@outlook.com>
2024-10-26 11:32:17 -0400

    add commit to pull

--- tictaketoe.js
+++ tictaketoe.js
@@ -1,6 +1,43 @@
-let player = 'x'
-const board = Array(9),full(null);
+// let xScore =0
+let check = 1
+// let oScore = 0
+// let tieScore = 0
 
-function yourturn(index){
-    const
+// function hit(selected){
+//     console.log("test")
+//     if (selected.textContent !== '') return;
+
+//     selected.textContent = currentPlayer
+//     if(checkForWin()){
+//         alert('you ${currentPlayer} win')
+//     } else if (currentPlayer === 'X') {
+//             xScore++ 
+//             oScore++
+//             resetGame()
+//     }
+    
+//     else if(isTie()){
+//         alert('You tied')
+//         tieScore++
+//         resetGame()
+//     }
+    
+// }
+function clicked(selected) {
+    if (check === 1) {
+        const element = document.querySelector('#' + selected.id);
+        element.textContent = 'X';
+        check++;
+    } else {
+        const element = document.querySelector('#' + selected.id);
+        element.textContent = 'O';
+        check--;
+    }   
+    
+}
+function checkWin(){
+    console.log('test')
+}
+function resetGame(){
+    console.log('test')
 }
